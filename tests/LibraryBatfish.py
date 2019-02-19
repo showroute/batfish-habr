7bf7416a59b27baa1101e851a5e86e519c8f40c3
showroute <46564285+showroute@users.noreply.github.com>
2019-02-19 17:43:35 +0300

    Update LibraryBatfish.py

--- tests/LibraryBatfish.py
+++ tests/LibraryBatfish.py
@@ -1,4 +1,4 @@
-from pybatfish.client.commands import *
+from pybatfish.client.commands import bf_logger, bf_init_snapshot
 from pybatfish.question.question import load_questions, list_questions
 from pybatfish.question import bfq
 from pybatfish.datamodel.flow import TcpFlags, MatchTcpFlags
